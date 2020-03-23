20468e6f21af2ca3e84943476e5f82cf142d60e7
Benjamin Karasik <benjamin@Benjamins-Air.home>
2020-03-23 02:48:29 -0400

    Updates

---
+++ precache-manifest.121790e89bc16438bbc396ea6a25ed0a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c68359ade75e843ed731920bf11ac47",
+    "url": "/jgogol-test-site/index.html"
+  },
+  {
+    "revision": "064296a0e430b82cdefa",
+    "url": "/jgogol-test-site/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "15ae487741b02d1ee9fa",
+    "url": "/jgogol-test-site/static/css/main.4abfa148.chunk.css"
+  },
+  {
+    "revision": "064296a0e430b82cdefa",
+    "url": "/jgogol-test-site/static/js/2.ddaecc6c.chunk.js"
+  },
+  {
+    "revision": "ca498560d6eb5a021164f950e20e0f60",
+    "url": "/jgogol-test-site/static/js/2.ddaecc6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ae487741b02d1ee9fa",
+    "url": "/jgogol-test-site/static/js/main.4d4fcb63.chunk.js"
+  },
+  {
+    "revision": "c07a7279c24455d53df9",
+    "url": "/jgogol-test-site/static/js/runtime-main.78a62d37.js"
+  },
+  {
+    "revision": "ba062b3048052bad7a7c1ea686fd19de",
+    "url": "/jgogol-test-site/static/media/jg1.ba062b30.jpg"
+  },
+  {
+    "revision": "a1507589c6c027fa6d359062170e1083",
+    "url": "/jgogol-test-site/static/media/jg2.a1507589.jpg"
+  },
+  {
+    "revision": "74528fe8606ab8ab4ef2e652ca22925e",
+    "url": "/jgogol-test-site/static/media/jg3.74528fe8.jpg"
+  },
+  {
+    "revision": "317381ce8cc7363b783bbcf71e1b1694",
+    "url": "/jgogol-test-site/static/media/jg4.317381ce.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jgogol-test-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
